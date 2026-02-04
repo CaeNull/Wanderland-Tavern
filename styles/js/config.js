47507a0a80b6c11e458ed1792a300a0c9356c865
EchoOfTheWolves <vickysun2004@gmail.com>
2026-02-04 21:24:04 +0800

    update URL

--- styles/js/config.js
+++ styles/js/config.js
@@ -16,7 +16,7 @@ let charadex = {};
 /* ==================================================================== */
 charadex.site = {
   title: "Charadex",
-  url: "https://charadex-team.github.io/charadex-v1.0/",
+  url: "https://caenull.github.io/Wanderland-Tavern/",
   description: `A tool for organizing small ARPGs and species.`
 }
 
