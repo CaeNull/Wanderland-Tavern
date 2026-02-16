f0c20166301fe499cc9fe425a5797c96761bfc38
EchoOfTheWolves <vickysun2004@gmail.com>
2026-02-16 23:23:07 +1100

    add facets badge..?

--- styles/js/charadex.js
+++ styles/js/charadex.js
@@ -37,6 +37,7 @@ charadex.initialize.page = async (dataArr, config, dataCallback, listCallback, c
     charadex.tools.addProfileLinks(entry, pageUrl, config.profileProperty); // Go ahead and add profile keys just in case
     if (folders) folders(entry, config.fauxFolder.folderProperty); // If folders, add folder info
     if (entry.rarity) entry.raritybadge = `<span class="badge badge-${charadex.tools.scrub(entry.rarity)}">${entry.rarity}</span>`; // Adds a rarity badge
+    if (entry.facets) entry.rarityfacets = `<span class="facets facets-${charadex.tools.scrub(entry.facets)}">${entry.facets}</span>`; // Adds a facets badge
   }
 
   // If there's related data, add it
